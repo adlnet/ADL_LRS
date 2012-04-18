0aca8d1cf9ce509706cdec13595056f3eeb394fc
Walt Grata <wegrata@gmail.com>
2012-04-18 14:13:59 -0400

    updated models relationships

--- lrs/models.py
+++ lrs/models.py
@@ -1,13 +1,34 @@
 from django.db import models
 from uuidfield import UUIDField
-class actor(models.Model):
+#this is BAD, if anyone knows a better way to store kv pairs in MySQL let me know
+
+class score(models.Model):
 	id = models.PositiveIntegerField(primary_key=True)
+	scaled = models.BooleanField()
+	raw = models.PositiveIntegerField()
+	min = models.PositiveIntegerField()
+	max = models.PositiveIntegerField()
+
+class state(models.Model):
+	key = models.PositiveIntegerField(primary_key=True)
+	id = models.CharField(max_length=200)
+	contents = models.CharField(max_length=200)
+
+class agent(models.Model):
+	id = models.PositiveIntegerField(primary_key=True)
+	objectType = models.CharField(max_length=200)
 
 class context(models.Model):
 	id = models.PositiveIntegerField(primary_key=True)
+	registration = UUIDField()
+	team = models.CharField(max_length=200)
+	contextActivities = models.CharField(max_length=200)
+	revision = models.CharField(max_length=200)
+	platform = models.CharField(max_length=200)
+	language = models.CharField(max_length=200)
+	statement = models.CharField(max_length=200)
 
-class activity(models.Model):
-	id = models.PositiveIntegerField(primary_key=True)
+
 
 class result(models.Model):
 	id = models.PositiveIntegerField(primary_key=True)	
@@ -16,15 +37,33 @@ class result(models.Model):
 	completion = models.BooleanField()
 	duration = models.DateTimeField()
 	extensions = models.CharField(max_length=200)
-# Create your models here.
+
 class statement(models.Model):
-	id = UUIDField(primary_key=True)
-	actor = models.ForeignKey(actor)
+	id = UUIDField(primary_key=True)	
 	verb = models.CharField(max_length=200)
-	inProgress = models.BooleanField()
-	object = models.ForeignKey(activity)
+	inProgress = models.BooleanField()	
 	result = models.ForeignKey(result)
 	timestamp = models.DateTimeField()
-	stored = models.DateTimeField()
-	#authority = models.ForeignKey(actor)
+	stored = models.DateTimeField()	
 	voided = models.BooleanField()
+
+class context_extentions(models.Model):
+	id = models.PositiveIntegerField(primary_key=True)
+	key=models.CharField(max_length=200)
+	value=models.CharField(max_length=200)
+	context = models.ForeignKey(context)
+class result_extension(models.Model):
+	id = models.PositiveIntegerField(primary_key=True)
+	key=models.CharField(max_length=200)
+	value=models.CharField(max_length=200)
+	result = models.ForeignKey(result)	
+class statement_actor(models.Model):
+	id = models.PositiveIntegerField(primary_key=True)
+	stmt = models.ForeignKey(statement)
+
+class context_actor(models.Model):
+	id = models.PositiveIntegerField(primary_key=True)
+	ctx = models.ForeignKey(context)	
+class activity(models.Model):
+	id = models.PositiveIntegerField(primary_key=True)
+	object = models.ForeignKey(statement)	
