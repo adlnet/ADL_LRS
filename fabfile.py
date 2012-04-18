a3284c4927fbc7afaf0d2c1bda0baceec5d132c0
Walt Grata <wegrata@gmail.com>
2012-04-18 12:16:14 -0400

    updated command to activate virtualenv

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,7 @@
 from fabric.api import local,run
-INSTALL_STEPS = ['yes | sudo apt-get install python-dev python-mysqldb python-virtualenv','virtualenv --no-site-packages env','source env/bin/activate;pip install -r requirements.txt']
+INSTALL_STEPS = ['yes | sudo apt-get install python-dev python-mysqldb python-virtualenv',
+                 'virtualenv --no-site-packages env',
+                 '. env/bin/activate;pip install -r requirements.txt']
 def deps_local():
     for step in INSTALL_STEPS:
     	local(step)
