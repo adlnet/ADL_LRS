c6eda30ff7b16b26d05eca1739e5537d714dc01f
Lou Wolford <lou.wolford.ctr@adlnet.gov>
2012-07-10 17:25:57 -0700

    added idnotfounderror

--- lrs/objects.py
+++ lrs/objects.py
@@ -304,6 +304,12 @@ class MultipleActorError(Exception):
     def __str__(self):
         return repr(self.message)
 
+class IDNotFoundError(Exception):
+    def __init__(self, msg):
+        self.message = msg
+    def __str__(self):
+        return repr(self.message)
+        
 class Activity():
 
     #activity definition required fields
