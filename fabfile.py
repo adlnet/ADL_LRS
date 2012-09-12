0131f2e3e82507b7a048e85ab2a3ca2051a10383
ljwolford <lou.wolford.ctr@adlnet.gov>
2012-09-12 16:40:37 -0300

    Update fabfile.py
    separated creating dirs and dependencies

--- fabfile.py
+++ fabfile.py
@@ -11,25 +11,26 @@ actor_profile = 'actor_profile'
 activity_profile = 'activity_profile'
 activity_state = 'activity_state'
 
-INSTALL_STEPS = ['yes | sudo apt-get install libmysqlclient-dev	 python-dev python-mysqldb python-virtualenv',
-                 'virtualenv --no-site-packages env',
-                 '. env/bin/activate;pip install -r requirements.txt']
+INSTALL_STEPS = ['yes | sudo apt-get install python-setuptools libmysqlclient-dev python-dev python-mysqldb python-libxml2 python-libxslt1 libxml2-dev libxslt1-dev',
+                 'sudo easy_install pip',
+                 'pip install -r requirements.txt']
 def deps_local():
     for step in INSTALL_STEPS:
     	local(step)
 
+def create_dirs():
     #Create media directories and give them open permissions
-	if not os.path.exists(path.join(adldir,activity_profile)):
-		os.makedirs(path.join(adldir,activity_profile))
-		os.chmod(path.join(adldir,activity_profile), 0777)
+    if not os.path.exists(path.join(adldir,activity_profile)):
+	os.makedirs(path.join(adldir,activity_profile))
+	os.chmod(path.join(adldir,activity_profile), 0777)
 
-	if not os.path.exists(path.join(adldir,activity_state)):
-		os.makedirs(path.join(adldir,activity_state))
-		os.chmod(path.join(adldir,activity_state), 0777)
+    if not os.path.exists(path.join(adldir,activity_state)):
+	os.makedirs(path.join(adldir,activity_state))
+	os.chmod(path.join(adldir,activity_state), 0777)
 
-	if not os.path.exists(path.join(adldir,actor_profile)):
-		os.makedirs(path.join(adldir,actor_profile))
-		os.chmod(path.join(adldir,actor_profile), 0777)
+    if not os.path.exists(path.join(adldir,actor_profile)):
+	os.makedirs(path.join(adldir,actor_profile))
+	os.chmod(path.join(adldir,actor_profile), 0777)
 
 def deps_remote():
     for step in INSTALL_STEPS:
