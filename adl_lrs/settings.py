e3a4c35460138b59a418085532b3bb2bbb56e3cd
ljwolford <lou.wolford.ctr@adlnet.gov>
2012-11-06 18:39:54 -0500

    changed settings/test for using postgres

--- adl_lrs/settings.py
+++ adl_lrs/settings.py
@@ -11,14 +11,19 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        # 'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'lrs',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': 'password',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': 'localhost',                      # NEED localhost for postgresql-Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'CLIENT_ENCODING': 'UTF8',
+        'DEFAULT_TRANSACTION_ISOLATION': 'read committed',
+        'TIMEZONE': 'UTC', 
     }
 }
+
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
