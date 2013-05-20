bb34fdcd80cabfbdce935b7165bdec899fb286e8
Lou Wolford <lou.wolford.ctr@adlnet.gov>
2013-05-20 11:34:10 -0400

    added cache table commands to fab

--- fabfile.py
+++ fabfile.py
@@ -1,40 +1,47 @@
-from fabric.api import local,run
 import os
-from os import path
-import pdb
 import sys
-
-def create_media():
-    agent_profile = 'agent_profile'
-    activity_profile = 'activity_profile'
-    activity_state = 'activity_state'
-    statement_attachments = 'attachment_payloads'
-    pdb.set_trace()
-    # Add settings module so fab file can see it
-    cwd = os.path.dirname(os.path.abspath(__file__))
-    sys.path.append(cwd)
-    env_dir = os.path.join(cwd, '../env/lib/python2.7/site-packages')
-    # sys.path.append('/home/lou/gitrepos/env/lib/python2.7/site-packages')
-    sys.path.append(env_dir)
-
-    os.environ['DJANGO_SETTINGS_MODULE'] = "adl_lrs.settings"
-    from django.conf import settings
-
-    adldir = settings.MEDIA_ROOT
-    #Create media directories
-    if not os.path.exists(path.join(adldir,activity_profile)):
-        os.makedirs(path.join(adldir,activity_profile))
-
-    if not os.path.exists(path.join(adldir,activity_state)):
-        os.makedirs(path.join(adldir,activity_state))
-
-    if not os.path.exists(path.join(adldir,agent_profile)):
-        os.makedirs(path.join(adldir,agent_profile))
-
-    if not os.path.exists(path.join(adldir,statement_attachments)):
-        os.makedirs(path.join(adldir,statement_attachments))
+from fabric.api import local,run
 
 def setup_env():
     INSTALL_STEPS = ['virtualenv ../env;. ../env/bin/activate;pip install -r requirements.txt;deactivate']
     for step in INSTALL_STEPS:
         local(step)
+
+def setup_lrs():
+    # Media folder names
+    agent_profile = 'agent_profile'
+    activity_profile = 'activity_profile'
+    activity_state = 'activity_state'
+    statement_attachments = 'attachment_payloads'
+    
+    # Add env packages and project to the path
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(cwd)
+    env_dir = os.path.join(cwd, '../env/lib/python2.7/site-packages')
+    sys.path.append(env_dir)
+
+    # Add settings module so fab file can see it
+    os.environ['DJANGO_SETTINGS_MODULE'] = "adl_lrs.settings"
+    from django.conf import settings
+    adldir = settings.MEDIA_ROOT
+
+    # Create media directories
+    if not os.path.exists(os.path.join(adldir,activity_profile)):
+        os.makedirs(os.path.join(adldir,activity_profile))
+
+    if not os.path.exists(os.path.join(adldir,activity_state)):
+        os.makedirs(os.path.join(adldir,activity_state))
+
+    if not os.path.exists(os.path.join(adldir,agent_profile)):
+        os.makedirs(os.path.join(adldir,agent_profile))
+
+    if not os.path.exists(os.path.join(adldir,statement_attachments)):
+        os.makedirs(os.path.join(adldir,statement_attachments))
+
+    # Create cache tables and sync the db
+    local('./manage.py createcachetable cache_statement_list')
+    local('./manage.py createcachetable attachment_cache')
+    local('./manage.py syncdb')
+
+def test_lrs():
+    local('./manage.py test lrs')
