d1e68f5b6ff1a36049095f09c1125e33a6fcdfb2
Lou Wolford <lou.wolford.ctr@adlnet.gov>
2013-11-26 09:15:08 -0500

    added root redirect

--- adl_lrs/urls.py
+++ adl_lrs/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import RedirectView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+	url(r'^$', RedirectView.as_view(url='/xAPI/')),
     url(r'^XAPI/', include('lrs.urls')),
     url(r'^xapi/', include('lrs.urls')),
     url(r'^xAPI/', include('lrs.urls')),
