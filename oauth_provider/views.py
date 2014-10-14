3e2ac736b6958478413298a06a5230090e462e1d
Lou Wolford <lou.wolford.ctr@adlnet.gov>
2014-10-14 10:46:39 -0400

    moved login/logout out of xapi routes

--- oauth_provider/views.py
+++ oauth_provider/views.py
@@ -56,9 +56,10 @@ def request_token(request):
     })
     return HttpResponse(ret, content_type='application/x-www-form-urlencoded')
 
+USER_AUTHORIZATION_FORM_CLASS = AuthClientForm
 # LRS CHANGE - CHANGED FORM_CLASS TO OUR CUSTOM FORM
-@login_required(login_url="/XAPI/accounts/login")
-def user_authorization(request, form_class=AuthClientForm):
+@login_required(login_url="/accounts/login")
+def user_authorization(request):
     if 'oauth_token' not in request.REQUEST:
         return HttpResponseBadRequest('No request token specified.')
 
@@ -76,7 +77,7 @@ def user_authorization(request, form_class=AuthClientForm):
         return HttpResponseForbidden('Invalid user for this client.')
 
     if request.method == 'POST':
-        form = form_class(request.POST)
+        form = USER_AUTHORIZATION_FORM_CLASS(request.POST)
         if request.session.get('oauth', '') == request_token.key and form.is_valid():
             request.session['oauth'] = ''
             if form.cleaned_data['authorize_access']:
@@ -212,7 +213,7 @@ def access_token(request):
     return HttpResponse(ret, content_type='application/x-www-form-urlencoded')
 
 # LRS CHANGE - ADDED OUR REAL VIEWS
-@login_required(login_url="/XAPI/accounts/login")
+@login_required(login_url="/accounts/login")
 def authorize_client(request, token=None, callback=None, params=None, form=None):
     if not form:
         form = AuthClientForm(initial={'scopes': token.scope_to_list(),
@@ -226,7 +227,7 @@ def authorize_client(request, token=None, callback=None, params=None, form=None)
     d['oauth_token'] = token.key
     return render_to_response('oauth_authorize_client.html', d, context_instance=RequestContext(request))
 
-@login_required(login_url="/XAPI/accounts/login")
+@login_required(login_url="/accounts/login")
 def callback_view(request, **args):
     d = {}
     if 'error' in args:
