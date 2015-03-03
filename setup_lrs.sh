eb21cbce0e31767296267335e32e4af9815ea282
Tyler Mulligan <tyler.mulligan.ctr@adlnet.gov>
2015-03-03 14:54:14 -0500

    stay in current directory

--- setup_lrs.sh
+++ setup_lrs.sh
@@ -11,7 +11,7 @@
 if [[ $(/usr/bin/id -u) -ne 0 ]]; then echo "This script must be run as root"; exit; fi
 
 echo ">>> Installing dependencies..."
-apt-get install fabric postgresql-9.1 python-setuptools postgresql-server-dev-9.1 python-dev libxml2-dev libxslt-dev
+apt-get install -y fabric postgresql-9.1 python-setuptools postgresql-server-dev-9.1 python-dev libxml2-dev libxslt-dev
 
 echo ">>> Installing pip with easy_install..."
 easy_install pip
@@ -24,7 +24,7 @@ sudo -u postgres createuser -s -P -e root
 sudo -u postgres psql -c 'CREATE DATABASE lrs OWNER root TEMPLATE template1'
 
 echo -e "Dependencies installed and database prepared. Finish installation as adllrs user: \n\n"
-echo "su - adllrs"
+echo "su adllrs"
 echo "fab setup_env"
 echo "source ../env/bin/activate"
 echo "fab setup_lrs"
