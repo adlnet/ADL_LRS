48f6e5c76a430d6c874b73c86719f905b928d6da
Lou Wolford <ljwolford@users.noreply.github.com>
2016-03-21 15:40:21 -0400

    Update AllowOriginMiddleware.py

--- adl_lrs/utils/AllowOriginMiddleware.py
+++ adl_lrs/utils/AllowOriginMiddleware.py
@@ -13,7 +13,6 @@ class AllowOriginMiddleware(object):
             response['Access-Control-Allow-Origin'] = "%s://%s:%s" % (protocol, request.META['SERVER_NAME'], port)
         else:
             response['Access-Control-Allow-Origin'] = "%s://%s" % (protocol, request.META['SERVER_NAME'])
-        response['Access-Control-Allow-Origin'] = '*'
         response['Access-Control-Allow-Methods'] = 'HEAD, POST, GET, OPTIONS, DELETE, PUT'
         response['Access-Control-Allow-Headers'] = 'Content-Type,Content-Length,Authorization,If-Match,If-None-Match,X-Experience-API-Version, Accept-Language'
         response['Access-Control-Expose-Headers'] = 'ETag,Last-Modified,Cache-Control,Content-Type,Content-Length,WWW-Authenticate,X-Experience-API-Version, Accept-Language'
