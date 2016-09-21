b94d24096436eb4edd156ac12e54f2181a04b00f
Lou Wolford <lou.wolford.ctr@adlnet.gov>
2016-09-21 13:47:17 -0400

    updated current xapi version

--- adl_lrs/settings.py
+++ adl_lrs/settings.py
@@ -90,9 +90,9 @@ STATICFILES_DIRS = (
 )
 
 # Current xAPI version
-XAPI_VERSION = '1.0.2'
+XAPI_VERSION = '1.0.3'
 
-XAPI_VERSIONS = ['1.0.0', '1.0.1', '1.0.2']
+XAPI_VERSIONS = ['1.0.0', '1.0.1', '1.0.2', XAPI_VERSION]
 
 # Where to be redirected after logging in
 LOGIN_REDIRECT_URL = '/me'
