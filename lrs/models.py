b894150a20090bf61b2f7489881b8f5f578bdf26
Argenrost <happycasanova@gmail.com>
2021-04-09 02:10:18 -0400

    LRS context agent/group debugging.

--- lrs/models.py
+++ lrs/models.py
@@ -380,14 +380,10 @@ class SubStatement(models.Model):
         Activity, related_name="sub_context_ca_other")
     context_contextAgent = models.ForeignKey(Agent, blank=True, null=True, on_delete=models.SET_NULL,
         db_index=True, related_name='sub_context_contextAgent')
-    # context_contextAgent = models.ForeignKey(
-    #     Agent, related_name="sub_context_contextAgent", null=True, on_delete=models.SET_NULL, db_index=True)
     context_contextAgent_relevantType = models.CharField(
         max_length=MAX_URL_LENGTH, blank=True, db_index=True)
     context_contextGroup = models.ForeignKey(Agent, blank=True, null=True, on_delete=models.SET_NULL,
         db_index=True, related_name='sub_context_contextGroup')
-    # context_contextGroup = models.ForeignKey(
-    #     Agent, related_name="sub_context_contextGroup", null=True, on_delete=models.SET_NULL, db_index=True)
     context_contextGroup_relevantType = models.CharField(
         max_length=MAX_URL_LENGTH, blank=True, db_index=True)
     # Context also has a stmt field which is a statementref.
@@ -561,14 +557,14 @@ class Statement(models.Model):
         Activity, related_name="stmt_context_ca_category")
     context_ca_other = models.ManyToManyField(
         Activity, related_name="stmt_context_ca_other")
-    context_contextAgent = models.ForeignKey(
-        Agent, related_name="stmt_context_contextAgent", null=True, on_delete=models.SET_NULL, db_index=True)
+    context_contextAgent = models.ForeignKey(Agent, blank=True, null=True, on_delete=models.SET_NULL,
+        db_index=True, related_name='sub_context_contextAgent')
     context_contextAgent_relevantType = models.CharField(
-        max_length=MAX_URL_LENGTH, db_index=True, unique=True)
-    context_contextGroup = models.ForeignKey(
-        Agent, related_name="stmt_context_contextGroup", null=True, on_delete=models.SET_NULL, db_index=True)
+        max_length=MAX_URL_LENGTH, blank=True, db_index=True)
+    context_contextGroup = models.ForeignKey(Agent, blank=True, null=True, on_delete=models.SET_NULL,
+        db_index=True, related_name='sub_context_contextGroup')
     context_contextGroup_relevantType = models.CharField(
-        max_length=MAX_URL_LENGTH, db_index=True, unique=True)
+        max_length=MAX_URL_LENGTH, blank=True, db_index=True)
     # Context also has a stmt field which is a statementref.
     context_statement = models.CharField(max_length=40, blank=True)
     version = models.CharField(max_length=7)
