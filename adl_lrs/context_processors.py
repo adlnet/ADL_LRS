08132dc3c05617ca604098bf62ff2a2806319c0e
Trey <trey.hayden.ctr@adlnet.gov>
2022-07-13 14:50:44 -0400

    fixing context key

--- adl_lrs/context_processors.py
+++ adl_lrs/context_processors.py
@@ -4,5 +4,5 @@ def recaptcha_config(request):
     # return the value you want as a dictionnary. you may add multiple values in there.
     return {
         'use_recaptcha': settings.USE_GOOGLE_RECAPTCHA,
-        'google_recaptcha_site_key': settings.GOOGLE_RECAPTCHA_SITE_KEY
+        'google_recaptcha_site_key': settings.RECAPTCHA_PUBLIC_KEY
     }
