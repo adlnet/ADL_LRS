007adec13e8d914732889f2f2df43db6080a9a38
Trey Hayden <trey.hayden.ctr@adlnet.gov>
2023-05-22 14:15:58 -0400

    etag updates for Agent Profile Resource

--- lrs/managers/AgentProfileManager.py
+++ lrs/managers/AgentProfileManager.py
@@ -6,7 +6,7 @@ from django.core.exceptions import ValidationError
 from django.utils.timezone import utc
 
 from ..models import AgentProfile
-from ..exceptions import IDNotFoundError, ParamError
+from ..exceptions import IDNotFoundError, ParamError, BadRequest
 from ..utils import etag
 
 
@@ -30,90 +30,94 @@ class AgentProfileManager():
         p.save()
 
     def post_profile(self, request_dict):
-        # get/create profile
-        p, created = AgentProfile.objects.get_or_create(
-            profile_id=request_dict['params']['profileId'], agent=self.Agent)
 
-        post_profile = request_dict['profile']
+        # get/create profile
+        profile_record, created = AgentProfile.objects.get_or_create(profile_id=request_dict['params']['profileId'], agent=self.Agent)
+        profile_document_contents = request_dict['profile']
+
+        etag.check_modification_conditions(request_dict, profile_record, created, required=True)
+        
         # If incoming profile is application/json and if a profile didn't
         # already exist with the same agent and profileId
         if created:
-            etag.check_modification_conditions(request_dict, p, created, required=False)
-            p.json_profile = post_profile
-            p.content_type = "application/json"
-            p.etag = etag.create_tag(post_profile)
+            profile_record.json_profile = profile_document_contents
+            profile_record.content_type = "application/json"
+            profile_record.etag = etag.create_tag(profile_document_contents)
+        
+        elif profile_record.content_type != "application/json":
+            raise BadRequest("A matching non-JSON document already exists and cannot be merged or replaced.")
+        
+        elif "application/json" not in request_dict['headers']['CONTENT_TYPE']:
+            raise BadRequest("A non-JSON document cannot be used to update an existing JSON document.")
+
         # If incoming profile is application/json and if a profile already
         # existed with the same agent and profileId
         else:
 
-            ##
-            ## 1.0.3 Behaviour
-            ##
-            # orig_prof = json.loads(p.json_profile)
-            # post_profile = json.loads(post_profile)
-            # merged = json.dumps(
-            #     dict(list(orig_prof.items()) + list(post_profile.items())))
-            # p.json_profile = merged
-            # p.etag = etag.create_tag(merged)
+            previous_profile = json.loads(profile_record.json_profile)
+            updated_profile = json.loads(profile_document_contents)
 
+            previous_profile_properties = list(previous_profile.items())
+            updated_profile_properties = list(updated_profile.items())
 
-            ##
-            ## 2.0 Behaviour to mirror PUT behaviour.
-            ##
-            # (overwrite existing profile data)
-            etag.check_modification_conditions(request_dict, p, created)
-            the_profile = request_dict['profile']
-            p.json_profile = the_profile
-            p.content_type = request_dict['headers']['CONTENT_TYPE']
-            p.etag = etag.create_tag(the_profile)
+            merged = json.dumps(dict(previous_profile_properties + updated_profile_properties))
 
+            profile_record.json_profile = merged
+            profile_record.content_type = request_dict['headers']['CONTENT_TYPE']
+            profile_record.etag = etag.create_tag(merged)
+        
         # Set updated
         if 'updated' in request_dict['headers'] and request_dict['headers']['updated']:
-            p.updated = request_dict['headers']['updated']
+            profile_record.updated = request_dict['headers']['updated']
         else:
-            p.updated = datetime.datetime.utcnow().replace(tzinfo=utc)
+            profile_record.updated = datetime.datetime.utcnow().replace(tzinfo=utc)
         
-        p.save()
+        profile_record.save()
 
     def put_profile(self, request_dict):
         # get/create profile
-        p, created = AgentProfile.objects.get_or_create(
-            profile_id=request_dict['params']['profileId'], agent=self.Agent)
+        profile_record, created = AgentProfile.objects.get_or_create(profile_id=request_dict['params']['profileId'], agent=self.Agent)
+
+        profile_document_contents = request_dict['state']
+
+        etag.check_modification_conditions(request_dict, profile_record, created, required=True)
 
         # Profile being PUT is not json
         if "application/json" not in request_dict['headers']['CONTENT_TYPE']:
             try:
-                profile = ContentFile(request_dict['profile'].read())
+                profile = ContentFile(profile_document_contents.read())
             except:
                 try:
-                    profile = ContentFile(request_dict['profile'])
+                    profile = ContentFile(profile_document_contents)
                 except:
-                    profile = ContentFile(str(request_dict['profile']))
+                    profile = ContentFile(str(profile_document_contents))
 
             # If it already exists delete it
-            if p.profile:
-                try:
-                    p.profile.delete()
-                except OSError:
-                    # probably was json before
-                    p.json_profile = {}
-            self.save_non_json_profile(p, profile, request_dict)
+            if not created:
+                if profile_record.profile:
+                    try:
+                        profile_record.profile.delete()
+                    except OSError:
+                        # probably was json before
+                        profile_record.json_profile = {}
+            
+            self.save_non_json_profile(profile_record, profile, request_dict)
+        
         # Profile being PUT is json
         else:
             # (overwrite existing profile data)
-            etag.check_modification_conditions(request_dict, p, created)
             the_profile = request_dict['profile']
-            p.json_profile = the_profile
-            p.content_type = request_dict['headers']['CONTENT_TYPE']
-            p.etag = etag.create_tag(the_profile)
+            profile_record.json_profile = the_profile
+            profile_record.content_type = request_dict['headers']['CONTENT_TYPE']
+            profile_record.etag = etag.create_tag(the_profile)
 
             # Set updated
             if 'updated' in request_dict['headers'] and request_dict['headers']['updated']:
-                p.updated = request_dict['headers']['updated']
+                profile_record.updated = request_dict['headers']['updated']
             else:
-                p.updated = datetime.datetime.utcnow().replace(tzinfo=utc)
+                profile_record.updated = datetime.datetime.utcnow().replace(tzinfo=utc)
             
-            p.save()
+            profile_record.save()
 
     def get_profile(self, profile_id):
         try:
