ec58582d04c08b16cb7d1c55d0a3f878c9617d2b
aadeyemiadl <147636785+aadeyemiadl@users.noreply.github.com>
2024-03-06 15:28:48 -0500

    Update install-lint.sh

--- install-lint.sh
+++ install-lint.sh
@@ -2,30 +2,38 @@
 
 # Simple script to install and run Hadolint
         echo ""
-	        echo "#====================================================#"
-		        echo "#            Installing Hadolint"
-			        echo "#====================================================#"
-				        echo ""
+	echo "#====================================================#"
+	echo "#        Installing Hadolint"
+	echo "#====================================================#"
+	echo ""
 
-					# Install Hadolint
-					        wget -O hadolint https://github.com/hadolint/hadolint/releases/download/v2.12.0/hadolint-Linux-x86_64
-						# Move to appropriate directory
-						        mv hadolint /usr/local/bin/hadolint
-							# Make file executable
-							        chmod +x /usr/local/bin/hadolint
-								# Check hadolint version to make sure it is installed
-								        hadolint -v
+# Install Hadolint
+	 wget -O hadolint https://github.com/hadolint/hadolint/releases/download/v2.12.0/hadolint-Linux-x86_64
+# Move to appropriate directory
+	mv hadolint /usr/local/bin/hadolint
+# Make file executable
+	chmod +x /usr/local/bin/hadolint
 
-									        echo ""
-										        echo "#====================================================#"
-											        echo "#            Hadolint Installation Complete"
-												        echo "#====================================================#"
-													        echo ""
-														# Run Hadolint on Dockerfile
-														        docker run --rm -i hadolint/hadolint < Dockerfile
+	echo ""
+	echo "#====================================================#"
+	echo "#        Hadolint Installation Complete"
+	echo "#====================================================#"
+	echo ""
+ 
+# Check hadolint version to make sure it is installed
+	hadolint -v
 
-															        echo ""
-																        echo "#====================================================#"
-																	        echo "#            Dockerfile Linting Complete"
-																		        echo "#====================================================#"
-																			        echo ""
+	echo ""
+	echo "#====================================================#"
+	echo "#       Now Running Hadolint on Dockerfile"
+	echo "#====================================================#"
+	echo ""
+ # Run Hadolint on Dockerfile
+														       
+	docker run --rm -i hadolint/hadolint < Dockerfile
+
+	echo ""
+	echo "#====================================================#"
+	echo "#      Dockerfile Linting Complete (Comments Above)"
+	echo "#====================================================#"
+	echo ""
