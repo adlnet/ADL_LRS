324046878b82973e468e8f2b51f6760a04ec2870
Trey <trey.hayden.ctr@adlnet.gov>
2024-03-08 15:59:15 -0500

    updating cypress config

--- tests/cypress/cypress.local.config.js
+++ tests/cypress/cypress.local.config.js
@@ -3,12 +3,12 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     specPattern: 'e2e/*.cy.{js,jsx,ts,tsx}',
-    baseUrl: process.env.LRS_HOSTNAME,
+    baseUrl: 'https://lrs.local.adlnet.gov',
     experimentalStudio: true
   },
   env: {
-    LRS_USERNAME: process.env.LRS_USERNAME,
-    LRS_PASSWORD: process.env.LRS_PASSWORD
+    LRS_USERNAME: "root",
+    LRS_PASSWORD: "1234"
   },
   defaultCommandTimeout: 9000,
 });
