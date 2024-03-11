dd75a492dcb140aeb3ca8828924bba1bf0839e8d
Ubuntu <ubuntu@ip-172-31-1-106.ec2.internal>
2024-03-11 17:21:55 +0000

    testing

--- newfile.js
+++
@@ -1,9 +0,0 @@
-// include node fs module
-var fs = require('fs');
-var data ='Learn Node FS module';
-  
-// writeFile function with filename, content and callback function
-fs.writeFile('newfile.txt', data, function (err) {
-	if (err) throw err;
-	console.log('File is created successfully.');
-});
