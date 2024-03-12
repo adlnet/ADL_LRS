ef477904354ee2dc2b492e7e2f640c2293127fce
aadeyemiadl <147636785+aadeyemiadl@users.noreply.github.com>
2024-03-12 16:59:15 -0400

    Update install-lint.sh
    added eslint to script

--- install-lint.sh
+++ install-lint.sh
@@ -71,3 +71,47 @@
 	echo "#      Python Linting Complete (Comments Above)"
 	echo "#====================================================#"
 	echo ""
+
+ # Simple script to install and run ESlint
+        echo ""
+	echo "#====================================================#"
+	echo "#        Installing and Configuring ESlint
+	echo "#====================================================#"
+	echo ""
+
+# Install npm if not already intsalled
+	sudo apt-get install npm
+# Install eslint if not already intsalled
+	npm install --save-dev eslint
+	sudo apt install eslint -g
+
+	chmod -R a+x node_modules
+
+# Initialize config file if not already done
+	npm init
+	npm init @eslint/config
+
+       
+	echo ""
+	echo "#====================================================#"
+	echo "#        ESlint Installation Complete"
+	echo "#====================================================#"
+	echo ""
+ 
+# Check ESlint version to make sure it is installed
+	eslint -v
+
+	echo ""
+	echo "#====================================================#"
+	echo "#       Now Running ESlint"
+	echo "#====================================================#"
+	echo ""
+ 
+ # Run ESlint on NodeJS files
+	eslint ./
+ 
+	echo ""
+	echo "#====================================================#"
+	echo "#      NodeJS Linting Complete (Comments Above)"
+	echo "#====================================================#"
+	echo ""
